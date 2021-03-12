8dcb1d8ac680e94874afc242aaa881ab4b191578
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-03-12 23:22:42 +0400

    start

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.config import Config
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scatter import Scatter
+
 
 Config.set('graphics', 'resizable', '0')
 Config.set('graphics', 'width', '500')
@@ -9,7 +12,15 @@ Config.set('graphics', 'height', '500')
 
 class MyApp(App):
     def build(self):
-        return Button(text = 'текст')
+        s = Scatter()
+        fl = FloatLayout(size=(300, 300))
+        s.add_widget(fl)
+        fl.add_widget(Button(text='текст',
+                             background_color=[.32, .85, .94, 1],
+                             background_normal='',
+                             size_hint=(.5, .25),
+                             pos=(0, 0)))
+        return s
 
 
 if __name__ == "__main__":
