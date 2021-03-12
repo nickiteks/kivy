f7978df4944592bf13511471acba52bb61d83cc9
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-03-12 22:25:26 +0400

    start

---
+++ main.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.config import Config
+
+Config.set('graphics', 'resizable', '0')
+Config.set('graphics', 'width', '500')
+Config.set('graphics', 'height', '500')
+
+
+class MyApp(App):
+    def build(self):
+        return Button()
+
+
+if __name__ == "__main__":
+    MyApp().run()
